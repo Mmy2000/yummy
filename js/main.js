b3df5937f702e5e0b19ebc00d277487547ffa138
Mmy2000 <yahianada005@gmail.com>
2024-04-26 21:39:21 +0300

    starting with js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
-$('#openBtn').click( ()=>{
-    $('.side-nav').animate({width:'260px'},()=>{
-        for (let i = 0; i < 5; i++) {
-        $(".taps li").eq(i).animate({
-            top: 0
-        }, (i + 5) * 100)
-    }
-    })
-    $('.close').animate({left:'260px'})
-    $('#openBtn').addClass('d-none');
-    $('#closeBtn').removeClass('d-none');
-})
-$('#closeBtn').click( ()=>{
-    $('.side-nav').animate({width:'0px'},()=>{
-        $(".taps li").animate({
-        top: 300
-    }, 500)
-    })
-    $('.close').animate({left:'0px'})
-    $('#openBtn').removeClass('d-none');
-    $('#closeBtn').addClass('d-none');
-})
+function openBtn() {
+        $('.side-nav').animate({
+            width: '260px'
+        }, () => {
+            for (let i = 0; i < 5; i++) {
+                $(".taps li").eq(i).animate({
+                    top: 0
+                }, (i + 5) * 100)
+            }
+        })
+        $('.close').animate({
+            left: '260px'
+        })
+        $('#openBtn').addClass('d-none');
+        $('#closeBtn').removeClass('d-none');
+}
+
+function closeBtn() {
+        $('.side-nav').animate({
+            width: '0px'
+        }, () => {
+            $(".taps li").animate({
+                top: 300
+            }, 500)
+        })
+        $('.close').animate({
+            left: '0px'
+        })
+        $('#openBtn').removeClass('d-none');
+        $('#closeBtn').addClass('d-none');
+}
