e80ba502b09ae60b5f8dc1c989f73eb18faa4be1
Mmy2000 <yahianada005@gmail.com>
2024-04-27 20:16:32 +0300

    starting with js

--- js/main.js
+++ js/main.js
@@ -98,7 +98,7 @@ function displayMeals(arr) {
         cartoona += `
         <div class="col-md-3">
                     <div class="inner">
-                        <div class="img position-relative overflow-hidden rounded-2">
+                        <div onclick="getMealDetails('${arr[i].idMeal}')" class="img position-relative overflow-hidden rounded-2">
                             <img class="w-100" src="${arr[i].strMealThumb}" alt="">
                             <div class="cover position-absolute  d-flex align-items-center justify-content-center p-5">
                                 <h2>${arr[i].strMeal}</h2>
@@ -421,4 +421,76 @@ function passwordValidation() {
 
 function repasswordValidation() {
     return document.getElementById("inputRepPassword").value == document.getElementById("inputPassword").value
+}
+
+async function getMealDetails(mealID) {
+    rowData.innerHTML = ""
+
+    searchContainer.innerHTML = "";
+    let respone = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`);
+    respone = await respone.json();
+
+    displayMealDetails(respone.meals[0])
+
+}
+function displayMealDetails(meal) {
+    
+    searchContainer.innerHTML = "";
+
+
+    let ingredients = ``
+
+    for (let i = 1; i <= 20; i++) {
+        if (meal[`strIngredient${i}`]) {
+            ingredients += `<li class="alert alert-info m-2 p-1">${meal[`strMeasure${i}`]} ${meal[`strIngredient${i}`]}</li>`
+        }
+    }
+
+    let tags = meal.strTags?.split(",")
+    // let tags = meal.strTags.split(",")
+    if (!tags) tags = []
+
+    let tagsStr = ''
+    for (let i = 0; i < tags.length; i++) {
+        tagsStr += `
+        <li class="alert alert-danger m-2 p-1">${tags[i]}</li>`
+    }
+
+
+
+    let cartoona = `
+    <div class="col-md-4 text-white text-start">
+                    <div class="inner">
+                        <img class="w-100" src="${meal.strMealThumb}" alt="">
+                        <h3>${meal.strMeal}</h3>
+
+                    </div>
+                </div>
+                <div class="col-md-8 text-white text-start">
+                    <h2>Instructions</h2>
+                    <p>${meal.strInstructions}</p>
+                    <h3>
+                        <span class="fw-bolder">Category : </span>
+                        ${meal.strCategory}
+                    </h3>
+                    <h3>
+                        <span class="fw-bolder">Area : </span>
+                        ${meal.strArea}
+                    </h3>
+                    <h3>Recipes :</h3>
+                    <ul class="list-unstyled d-flex g-3 flex-wrap">
+                        ${ingredients}
+                        
+                    </ul>
+                    <h3>Tags :</h3>
+                    <ul class="list-unstyled d-flex g-3 flex-wrap">
+                        ${tagsStr}
+                        
+                    </ul>
+                    <a target="_blank" href="${meal.strSource}" class="btn btn-success">Source</a>
+                <a target="_blank" href="${meal.strYoutube}" class="btn btn-danger">Youtube</a>
+
+                </div>`
+
+    rowData.innerHTML = cartoona
 }
