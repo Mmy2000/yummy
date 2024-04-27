0e97b8c4d741f22bc33e171c2a3e193c3a612cfb
Mmy2000 <yahianada005@gmail.com>
2024-04-27 18:33:15 +0300

    starting with js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ let showCat = document.getElementById("showCat");
 let showArea = document.getElementById("showArea");
 let showIngredients = document.getElementById("showIngredients");
 
+
 function openBtn() {
     $('.side-nav').animate({
         width: '260px'
@@ -251,35 +252,133 @@ async function getIngredientsMeals(ingredients) {
     displayMeals(response.meals.slice(0, 20))
 
 }
+let submitBtn = document.getElementById('submitBtn')
+let inputName = document.getElementById('inputName')
+let inputPhone = document.getElementById('inputPhone')
+let inputPassword = document.getElementById('inputPassword')
+let inputEmail = document.getElementById('inputEmail')
+let inputAge = document.getElementById('inputAge')
+let inputRepPassword = document.getElementById('inputRepPassword')
+inputName.addEventListener("focus", () => {
+        nameInputTouched = true
+    })
+
+    inputEmail.addEventListener("focus", () => {
+        emailInputTouched = true
+    })
+
+    inputPhone.addEventListener("focus", () => {
+        phoneInputTouched = true
+    })
+
+    inputAge.addEventListener("focus", () => {
+        ageInputTouched = true
+    })
+
+    inputPassword.addEventListener("focus", () => {
+        passwordInputTouched = true
+    })
+
+    inputRepPassword.addEventListener("focus", () => {
+        repasswordInputTouched = true
+    })
+
+let nameInputTouched = false;
+let emailInputTouched = false;
+let phoneInputTouched = false;
+let ageInputTouched = false;
+let passwordInputTouched = false;
+let repasswordInputTouched = false;
+
+function inputsValidation() {
+    if (nameInputTouched) {
+        if (nameValidation()) {
+            document.getElementById("nameAlert").classList.replace("d-block", "d-none")
+
+        } else {
+            document.getElementById("nameAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+    if (emailInputTouched) {
+
+        if (emailValidation()) {
+            document.getElementById("emailAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("emailAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+    if (phoneInputTouched) {
+        if (phoneValidation()) {
+            document.getElementById("phoneAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("phoneAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+    if (ageInputTouched) {
+        if (ageValidation()) {
+            document.getElementById("ageAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("ageAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+
+    if (passwordInputTouched) {
+        if (passwordValidation()) {
+            document.getElementById("passwordAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("passwordAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
+    if (repasswordInputTouched) {
+        if (repasswordValidation()) {
+            document.getElementById("repasswordAlert").classList.replace("d-block", "d-none")
+        } else {
+            document.getElementById("repasswordAlert").classList.replace("d-none", "d-block")
+
+        }
+    }
 
 
-
-
-
-
-
+    if (nameValidation() &&
+        emailValidation() &&
+        phoneValidation() &&
+        ageValidation() &&
+        passwordValidation() &&
+        repasswordValidation()) {
+        submitBtn.removeAttribute("disabled")
+    } else {
+        submitBtn.setAttribute("disabled", true)
+    }
+}
 
 
 function nameValidation() {
-    return (/^[a-zA-Z ]+$/.test(document.getElementById("nameInput").value))
+    return (/^[a-zA-Z ]+$/.test(document.getElementById("inputName").value))
 }
 
 function emailValidation() {
-    return (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(document.getElementById("emailInput").value))
+    return (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(document.getElementById("inputEmail").value))
 }
 
 function phoneValidation() {
-    return (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(document.getElementById("phoneInput").value))
+    return (/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(document.getElementById("inputPhone").value))
 }
 
 function ageValidation() {
-    return (/^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|200)$/.test(document.getElementById("ageInput").value))
+    return (/^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|200)$/.test(document.getElementById("inputAge").value))
 }
 
 function passwordValidation() {
-    return (/^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{8,}$/.test(document.getElementById("passwordInput").value))
+    return (/^(?=.*\d)(?=.*[a-z])[0-9a-zA-Z]{8,}$/.test(document.getElementById("inputPassword").value))
 }
 
 function repasswordValidation() {
-    return document.getElementById("repasswordInput").value == document.getElementById("passwordInput").value
+    return document.getElementById("inputRepPassword").value == document.getElementById("inputPassword").value
 }
